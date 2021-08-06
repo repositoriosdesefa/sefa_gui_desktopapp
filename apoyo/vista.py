3eab0618a45432a25788aca4e8ffd0b06c026385
proyectoseinnovacion <proyectossefa@oefa.gob.pe>
2021-08-06 12:16:23 -0500

    Primer avance

--- apoyo/vista.py
+++ apoyo/vista.py
@@ -1,238 +1,151 @@
-import tkinter as tk
-from tkinter import Frame, Canvas, Label
-import apoyo.formato as formato 
+# I. Librerias
+import gspread
 import pandas as pd
+import tkinter as tk
+from tkinter import messagebox
+from tkcalendar import Calendar, DateEntry
+import datetime
+from elemetos_de_GUI import *
 
-class VerticalScrolledFrame:
-    """
-    From: https://gist.github.com/novel-yet-trivial/3eddfce704db3082e38c84664fc1fdf8
-    A vertically scrolled Frame that can be treated like any other Frame
-    ie it needs a master and layout and it can be a master.
-    :width:, :height:, :bg: are passed to the underlying Canvas
-    :bg: and all other keyword arguments are passed to the inner Frame
-    note that a widget layed out in this frame will have a self.master 3 layers deep,
-    (outer Frame, Canvas, inner Frame) so 
-    if you subclass this there is no built in way for the children to access it.
-    You need to provide the controller separately.
-    """
-    def __init__(self, master, **kwargs):
-        width = kwargs.pop('width', None)
-        height = kwargs.pop('height', None)
-        bg = kwargs.pop('bg', kwargs.pop('background', None))
-        self.outer = Frame(master, **kwargs)
+# Tabla
+t_doc_rec = {
+    'Categoria': ['Oficio', 'Reiterativo'],
+    'HT': ['2021-E01-234102', '2021-E01-0335102'],
+    'Destinatario': ['DGM', 'Metropolitana de Lima']
+}
 
-        self.vsb = tk.Scrollbar(self.outer, orient=tk.VERTICAL)
-        self.vsb.pack(fill=tk.Y, side=tk.RIGHT)
-        self.canvas = tk.Canvas(self.outer, highlightthickness=0, width=width, height=height, bg=bg)
-        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        self.canvas['yscrollcommand'] = self.vsb.set
-        # mouse scroll does not seem to work with just "bind"; You have
-        # to use "bind_all". Therefore to use multiple windows you have
-        # to bind_all in the current widget
-        self.canvas.bind("<Enter>", self._bind_mouse)
-        self.canvas.bind("<Leave>", self._unbind_mouse)
-        self.vsb['command'] = self.canvas.yview
+tabla = pd.DataFrame(t_doc_rec)
 
-        self.inner = tk.Frame(self.canvas, bg=bg)
-        # pack the inner Frame into the Canvas with the topleft corner 4 pixels offset
-        self.canvas.create_window(4, 4, window=self.inner, anchor='nw')
-        self.inner.bind("<Configure>", self._on_frame_configure)
 
-        self.outer_attr = set(dir(tk.Widget))
 
-    def __getattr__(self, item):
-        if item in self.outer_attr:
-            # geometry attributes etc (eg pack, destroy, tkraise) are passed on to self.outer
-            return getattr(self.outer, item)
-        else:
-            # all other attributes (_w, children, etc) are passed to self.inner
-            return getattr(self.inner, item)
+# II. Funciones
 
-    def _on_frame_configure(self, event=None):
-        x1, y1, x2, y2 = self.canvas.bbox("all")
-        height = self.canvas.winfo_height()
-        self.canvas.config(scrollregion = (0,0, x2, max(y2, height)))
+# o) Obtener dato
 
-    def _bind_mouse(self, event=None):
-        self.canvas.bind_all("<4>", self._on_mousewheel)
-        self.canvas.bind_all("<5>", self._on_mousewheel)
-        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
 
-    def _unbind_mouse(self, event=None):
-        self.canvas.unbind_all("<4>")
-        self.canvas.unbind_all("<5>")
-        self.canvas.unbind_all("<MouseWheel>")
+# i) Validación de números
+def valnum(digito):
+    if digito.isdigit():
+	    return True
+    elif digito == "":
+	    return True
+    else:
+	    return False
 
-    def _on_mousewheel(self, event):
-        """Linux uses event.num; Windows / Mac uses event.delta"""
-        if event.num == 4 or event.delta > 0:
-            self.canvas.yview_scroll(-1, "units" )
-        elif event.num == 5 or event.delta < 0:
-            self.canvas.yview_scroll(1, "units" )
+def valfecha(fecha1, fecha2):
+    if fecha1<fecha2:
+        return TRUE
+    else: 
+        raise messagebox.showerror("Error", "Inconsistencia de fechas. La fecha de carga del informe de supervisión no puede ser anterior a la fecha fin de supervisión.")
 
-    def __str__(self):
-        return str(self.outer)
+# iii) Registro de Información
+def enviar():
+    # Validación de fechas
+    valfecha(l2_valor1.get(),l2_valor2.get())
 
-class Vitrina(Frame):
-    """"""
+    # Conexión con Google Sheets
+    gc = gspread.service_account(filename='accesos.json')
+    sh = gc.open_by_key('13EgFGcKnHUomMtjBlgZOlPIg_cb4N3aGpkYH13zG6-4')
+    worksheet = sh.worksheet("DOC_RECIBIDOS")
 
-    #----------------------------------------------------------------------
-    def __init__(self, window, tabla, funcion1, funcion2, funcion3, height=100, width=1600):
-        """Constructor"""
+    # Obtención de valores
+    od = l1_valor1.get()
+    exp = l1_valor2.get()
+    #nivel = l1_valor3.get()
 
-        self.window = window
-        self.tabla = tabla
-        self.funcion1 = funcion1
-        self.funcion2 = funcion2
-        self.funcion3 = funcion3
-        self.height = height
-        self.width = width
-        
-        self.main_frame = Frame(self.window)
-        self.main_frame.pack()
+    f_fin = l2_valor1.get()
+    f_carga = l2_valor2.get()
+    objetivos_plan = l2_valor3.get()
+    objetivos_acta = l2_valor4.get()
 
-        # Encabezados (entorno)
+    # Subida de información
+    worksheet.append_row([od, exp, f_fin, f_carga, int(objetivos_plan), int(objetivos_acta)])
+    # Confirmación de registro
+    messagebox.showinfo("¡Excelente!", "El registro se ha ingresado correctamente")
 
-        self.frame_base_encabezado = Frame(self.main_frame)
-        self.frame_base_encabezado.pack()
+# III. Aplicativo
 
-        self.canvas_encabezado = Canvas(self.frame_base_encabezado, height=23, width=self.width)
-        self.canvas_encabezado.pack(side='left', fill='both', expand=True)
+# i) Apertura de ventana
+app_ospa = Tk()
 
-        self.empty_frame = Frame(self.frame_base_encabezado, width=12)
-        self.empty_frame.pack(side='right', fill='both', expand=True)
+# Interpretación Tcl a Python
+reg = app_ospa.register(valnum)
 
-        self.frame_dentro_del_canvas = Frame(self.canvas_encabezado) # Éste es el frame donde colocaremos los encabezados
+# Frames de la aplicación
+lienzo1 = Frame(app_ospa)
+lienzo1.pack()
 
-        self.canvas_encabezado.create_window(4, 4, window=self.frame_dentro_del_canvas, anchor='nw')
+lienzo2 = Frame(app_ospa)
+lienzo2.pack()
 
-        # Cuerpo de la tabla (entorno)
+lienzo3 = Cuadro(app_ospa)
 
-        self.frame_con_scrollbar = VerticalScrolledFrame(self.main_frame, height=self.height, width=self.width)
-        self.frame_con_scrollbar.pack()
+# ii) Objetos
+# Labels
+l1_titulo = Label(lienzo1, text="Ospa: Observatorio")
+l1_label1 = Label(lienzo1, text="HT entrante")
+l1_label2 = Label(lienzo1, text="Vía de recepción")
+# l1_label3 = Label(lienzo1, text="Aporte del documento")
 
-        # Incluir encabezados de la tabla
+# l2_titulo = Label(lienzo2, text="Otros")
+l2_label1 = Label(lienzo2, text="Fecha de ingreso OEFA")
+l2_label2 = Label(lienzo2, text="Fecha de ingreso SEFA")
+l2_label3 = Label(lienzo2, text="Tipo de documento")
+l2_label4 = Label(lienzo2, text="N° de documento")
 
-        columnas_de_tabla = self.tabla.columns.values.tolist()
-        indice_de_columnas = range(len(columnas_de_tabla))
-        elementos_columnas = {
-            'index': indice_de_columnas,
-            'columnas': columnas_de_tabla
-            }
-        tabla_columna = pd.DataFrame(elementos_columnas)
+# Valores
+l1_valor1 = StringVar()
+l1_valor2 = StringVar()
+# l1_valor3 = StringVar()
 
-        for i,row in tabla_columna.iterrows():
-            encabezado = Label(
-                self.frame_dentro_del_canvas, 
-                text=row[1],
-                font=formato.tipo_de_letra_tabla,
-                fg = formato.letras_del_boton,
-                width= 25, 
-                height=1, 
-                relief='groove',
-                bg=formato.fondo_encabezados_de_tabla
-                )
-            encabezado.grid(row=0, column=row[0])
-    
-        opciones_label = Label(
-            self.frame_dentro_del_canvas,
-            text='Opciones',
-            font=formato.tipo_de_letra_tabla,
-            fg = formato.letras_del_boton,
-            width= 16, 
-            height=1, 
-            relief='groove',
-            bg=formato.fondo_encabezados_de_tabla
-            )
-        opciones_label.grid(row=0, column=len(columnas_de_tabla)+1)
+l2_valor1 = StringVar()
+l2_valor2 = StringVar()
+l2_valor3 = StringVar()
+l2_valor4 = StringVar()
 
-        # Incluir elementos en el cuerpo de la tabla
+# Entries
+l1_entry1 = Entry(lienzo1, textvariable=l1_valor1, width= 20, borderwidth=2)
+l1_entry2 = Entry(lienzo1, textvariable=l1_valor2, width= 20, borderwidth=2)
+# l1_entry3 = Entry(lienzo1, textvariable=l1_valor3, width= 20, borderwidth=2)
 
-        for i, row in self.tabla.iterrows():
-            valores_subframe = Frame(self.frame_con_scrollbar)
-            valores_subframe.pack()
-            lista_de_valores = row.values
-            indice_de_valores = range(len(lista_de_valores))
-            elementos_valores = {
-                'index': indice_de_valores,
-                'valores': lista_de_valores 
-            }
-            tabla_valor = pd.DataFrame(elementos_valores)
-            for i,row in tabla_valor.iterrows():
-                valor = Label(
-                    valores_subframe, 
-                    text=row[1],
-                    font=formato.tipo_de_letra_tabla,
-                    width=25, 
-                    height=1, 
-                    relief='groove',
-                    bg= formato.fondo_valores_de_tabla)
-                valor.grid(row=0, column=row[0])
+l2_entry1 = DateEntry(lienzo2, textvariable=l2_valor1, background='darkblue', foreground='white', width= 9, borderwidth=2)
+l2_entry2 = DateEntry(lienzo2, textvariable=l2_valor2, background='darkblue', foreground='white', width= 9, borderwidth=2)
+l2_entry3 = Entry(lienzo2, textvariable=l2_valor3, width= 12, borderwidth=2)
+l2_entry3.config(validate="key", validatecommand=(reg, '%P'))
+l2_entry4 = Entry(lienzo2, textvariable=l2_valor4, width= 12, borderwidth=2)
+l2_entry4.config(validate="key", validatecommand=(reg, '%P'))
 
-            # Agregar botones
-            
-            argumento = lista_de_valores[0]
+# Botones
+boton = Button(lienzo2, text="Guardar", command=enviar)
 
-            boton_ver = Label(
-                valores_subframe,
-                text='VER',
-                font=formato.tipo_de_letra_tabla,
-                fg = formato.letras_del_boton,
-                width=5,
-                height=1,
-                relief='groove',
-                cursor="hand2",
-                bg=formato.boton_sin_que_pase_cursor
-            )
-            boton_ver.grid(row=0, column=len(lista_de_valores)+1)
-            boton_ver.bind("<Button-1>",lambda e,argumento=argumento:self.funcion1(argumento))
-            self.Efecto_de_boton(boton_ver)
+# Objetos definidos por elemetos de GUI
+lienzo3.agregar_escenario(2, 2, tabla, enviar, enviar, enviar)
 
-            boton_editar = Label(
-                valores_subframe,
-                text='EDIT',
-                font=formato.tipo_de_letra_tabla,
-                fg = formato.letras_del_boton,
-                width=5,
-                height=1,
-                relief='groove',
-                cursor="hand2",
-                bg=formato.boton_sin_que_pase_cursor
-            )
-            boton_editar.grid(row=0, column=len(lista_de_valores)+2)
-            boton_editar.bind("<Button-1>",lambda e,argumento=argumento:self.funcion2(argumento))
-            self.Efecto_de_boton(boton_editar)
+# Ubicaciones
+# Lienzo 1
+l1_titulo.grid(row= 0, column=0, columnspan=4)
+l1_label1.grid(row= 1, column=0)
+l1_label2.grid(row= 1, column=2)
+# l1_label3.grid(row= 3, column=0)
 
-            boton_eliminar = Label(
-                valores_subframe,
-                text='DEL',
-                font=formato.tipo_de_letra_tabla,
-                fg = formato.letras_del_boton,
-                width=5,
-                height=1,
-                relief='groove',
-                cursor="hand2",
-                bg=formato.boton_sin_que_pase_cursor
-            )
-            boton_eliminar.grid(row=0, column=len(lista_de_valores)+3)
-            boton_eliminar.bind("<Button-1>",lambda e,argumento=argumento:self.funcion3(argumento))
-            self.Efecto_de_boton(boton_eliminar)
+l1_entry1.grid(row= 1, column=1)
+l1_entry2.grid(row= 1, column=3)
+# l1_entry3.grid(row= 3, column=1)
 
-    #----------------------------------------------------------------------
-    def Efecto_de_boton(self, boton):
-        """"""
+# Lienzo 2
+# l2_titulo.grid(row= 0, column=0, columnspan=2)
+l2_label1.grid(row= 0, column=0)
+l2_label2.grid(row= 0, column=2)
+l2_label3.grid(row= 1, column=0)
+l2_label4.grid(row= 1, column=2)
 
-        #----------------------------------------------------------------------
-        def Pasar_sobre_boton(e):
-            """"""
-            boton['bg'] = formato.boton_cuando_pasa_cursor
-            boton['fg'] = formato.letras_del_boton_cuando_pasa_cursor
+l2_entry1.grid(row= 0, column=1)
+l2_entry2.grid(row= 0, column=3)
+l2_entry3.grid(row= 1, column=1)
+l2_entry4.grid(row= 1, column=3)
 
-        #----------------------------------------------------------------------
-        def Dejar_boton(e):
-            """"""
-            boton['bg'] = formato.boton_sin_que_pase_cursor
-            boton['fg'] = formato.letras_del_boton
+boton.grid(row= 2, column=0, columnspan=4)
 
-        boton.bind('<Enter>', Pasar_sobre_boton)
-        boton.bind('<Leave>', Dejar_boton)
+
+# iii) Loop para App
+app_ospa.mainloop()
