4ded99ac99d7bd253b5d61e8750d5e5991c4f670
proyectoseinnovacion <proyectossefa@oefa.gob.pe>
2021-08-31 17:37:12 -0500

    Actualización del filtro de fecha

--- modulos/busqueda_dr.py
+++ modulos/busqueda_dr.py
@@ -9,6 +9,7 @@ from apoyo.manejo_de_bases import Base_de_datos
 from apoyo.vsf import Vitrina_busqueda
 import apoyo.datos_frecuentes as dfrec
 from modulos import vista_dr
+import datetime
 
 class Doc_recibidos_busqueda(Ventana):
     """"""
@@ -28,10 +29,9 @@ class Doc_recibidos_busqueda(Ventana):
         self.listatipodoc = list(set(self.tabla_inicial2['Tipodedocumento']))
         self.listadestina = list(set(self.tabla_inicial2['Remitente']))
         self.listaht = list(set(self.tabla_inicial2['HTEntrante']))
-        #self.listadestina.unique()
-        #self.listatipodoc.unique()
 
-        rejilla_dr = (
+
+        self.rejilla_dr = (
             ('L', 0, 0, 'Tipo de documento'),
             ('CX', 0, 1, self.listatipodoc),
 
@@ -42,38 +42,20 @@ class Doc_recibidos_busqueda(Ventana):
             ('E', 0, 3),
 
             ('L', 1, 2, 'Fecha de recepción OEFA'),
-            ('E', 1, 3)
+            ('D', 1, 3)
 
         )
+        
+
 
         self.c0 = Cuadro(self)
         self.c0.agregar_label(0,0,' ')
         self.c0.agregar_imagen(1,0,'Logo_OSPA.png',202,49)
 
         self.c1 = Cuadro(self)
-        self.c1.agregar_rejilla(rejilla_dr)
+        self.c1.agregar_rejilla(self.rejilla_dr)
 
-        #self.c1.obtener_dato(0)
-        #self.c1.obtener_dato(1)
-        #self.c1.obtener_dato(2)
 
-        #if len(self.c1.obtener_dato(0))>0 :
-        #    self.tabla_inicial3 = self.tabla_inicial2[self.tabla_inicial2['Tipodedocumento']==self.c1.obtener_dato(0)]
-        #    del self.listadestina
-        #    self.listadestina = list(set(self.tabla_inicial3['Remitente']))
-        #else:
-        #    if len(self.c1.obtener_dato(1))>0 :
-        #        self.tabla_inicial3 = self.tabla_inicial2[self.tabla_inicial2['Remitente']==self.c1.obtener_dato(1)]
-        #        self.listatipodoc = list(set(self.tabla_inicial3['Tipodedocumento']))
-        #    else:
-        #        self.tabla_inicial2       
-
-        #self.c16 = Cuadro(self)
-        #self.c16.agregar_rejilla(rejilla_dr)
-        #self.c16.obtener_dato(0)
-        
-        #self.c6 = Cuadro(self)
-        #self.c6.agregar_combobox(1,1,self.listadestina)
 
         rejilla_b = (
             ('B', 5, 4, 'Buscar', self.Buscar),
@@ -93,11 +75,13 @@ class Doc_recibidos_busqueda(Ventana):
 
     def Buscar(self):
 
-        listas_filtro = self.c1.obtener_lista_de_datos()
-        self.tipodoc = listas_filtro[0]
-        self.remitente = listas_filtro[1]
-        self.ht = listas_filtro[2]
-        self.fecha = listas_filtro[3]
+        self.listas_filtro = self.c1.obtener_lista_de_datos()
+        self.tipodoc = self.listas_filtro[0]
+        self.remitente = self.listas_filtro[1]
+        self.ht = self.listas_filtro[2]
+        fecha_original = self.listas_filtro[3]
+        objeto_fecha = datetime.datetime.strptime(fecha_original, '%Y-%m-%d')
+        self.fecha = str(objeto_fecha.strftime("%d/%m/%Y"))
 
         if self.tipodoc != "":
             self.v1.Eliminar_vitrina()
@@ -115,8 +99,7 @@ class Doc_recibidos_busqueda(Ventana):
                         self.v1.Eliminar_vitrina()
                         self.tabla_filtrada4 = self.tabla_filtrada3[self.tabla_filtrada3['Fecha de ingreso OEFA']==self.fecha]
                         self.v1 = Vitrina_busqueda(self, self.tabla_filtrada4, self.Buscar, self.funcion_de_prueba, height=200, width=1030)
-                    else:
-                        self.tabla_de_dr
+                    
                 else:
                     if self.fecha != "":
                         self.v1.Eliminar_vitrina()
@@ -133,18 +116,51 @@ class Doc_recibidos_busqueda(Ventana):
                         self.tabla_filtrada3 = self.tabla_filtrada2[self.tabla_filtrada2['Fecha de ingreso OEFA']==self.fecha]
                         self.v1 = Vitrina_busqueda(self, self.tabla_filtrada3, self.Buscar, self.funcion_de_prueba, height=200, width=1030)
                 else:
-                     if self.fecha != "":
+                    if self.fecha != "":
                         self.v1.Eliminar_vitrina()
                         self.tabla_filtrada2 = self.tabla_filtrada[self.tabla_filtrada['Fecha de ingreso OEFA']==self.fecha]
                         self.v1 = Vitrina_busqueda(self, self.tabla_filtrada2, self.Buscar, self.funcion_de_prueba, height=200, width=1030)
+                   
+        
         else:
-            self.tabla_de_dr
+            if self.remitente != "":
+                self.v1.Eliminar_vitrina()
+                self.tabla_filtrada = self.tabla_de_dr[self.tabla_de_dr['Remitente']==self.remitente]
+                self.v1 = Vitrina_busqueda(self, self.tabla_filtrada, self.Buscar, self.funcion_de_prueba, height=200, width=1030)
+                if self.ht != "":
+                    self.v1.Eliminar_vitrina()
+                    self.tabla_filtrada2 = self.tabla_filtrada[self.tabla_filtrada['N° registro Siged']==self.ht]
+                    self.v1 = Vitrina_busqueda(self, self.tabla_filtrada2, self.Buscar, self.funcion_de_prueba, height=200, width=1030)
+                    if self.fecha != "":
+                        self.v1.Eliminar_vitrina()
+                        self.tabla_filtrada3 = self.tabla_filtrada2[self.tabla_filtrada2['Fecha de ingreso OEFA']==self.fecha]
+                        self.v1 = Vitrina_busqueda(self, self.tabla_filtrada4, self.Buscar, self.funcion_de_prueba, height=200, width=1030)
+                else:
+                    if self.fecha != "":
+                        self.v1.Eliminar_vitrina()
+                        self.tabla_filtrada2 = self.tabla_filtrada[self.tabla_filtrada['Fecha de ingreso OEFA']==self.fecha]
+                        self.v1 = Vitrina_busqueda(self, self.tabla_filtrada, self.Buscar, self.funcion_de_prueba, height=200, width=1030)
+              
+            else:
+                if self.ht != "":
+                    self.v1.Eliminar_vitrina()
+                    self.tabla_filtrada = self.tabla_de_dr[self.tabla_de_dr['N° registro Siged']==self.ht]
+                    self.v1 = Vitrina_busqueda(self, self.tabla_filtrada, self.Buscar, self.funcion_de_prueba, height=200, width=1030)
+                    if self.fecha != "":
+                        self.v1.Eliminar_vitrina()
+                        self.tabla_filtrada2 = self.tabla_filtrada[self.tabla_filtrada['Fecha de ingreso OEFA']==self.fecha]
+                        self.v1 = Vitrina_busqueda(self, self.tabla_filtrada2, self.Buscar, self.funcion_de_prueba, height=200, width=1030)
+  
+                else:
+                    if self.fecha != "":
+                        self.v1.Eliminar_vitrina()
+                        self.tabla_filtrada = self.tabla_de_dr[self.tabla_de_dr['Fecha de ingreso OEFA']==self.fecha]
+                        self.v1 = Vitrina_busqueda(self, self.tabla_filtrada, self.Buscar, self.funcion_de_prueba, height=200, width=1030)
+    
 
     def limpiar(self):
-
-        #self.desaparecer()
-        #subframe = Doc_recibidos_vista(self, 500, 1300, 'Interfaz para el control de usuarios') 
-        #subframe = Doc_recibidos_vista(self, 500, 400, 'Nuevo usuario')
+        
+        
         self.v1.Eliminar_vitrina()
 
 
@@ -174,4 +190,3 @@ class Doc_recibidos_busqueda(Ventana):
       #      self.mostrar_datos()
 
        # self.mostrar_datos()
-       
